5707bac34d7404474c8be617e1005f7f4f52c9a5
iman hashemi <hashemi.iman@gmail.com>
2025-07-08 18:51:01 +0330

    categories

--- routes/admin.php
+++ routes/admin.php
@@ -1,14 +1,18 @@
 <?php
 
 
+use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\PanelController;
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[PanelController::class,'index'])->name('panel.index');
+
 Route::get('/users',[UserController::class,'index'])->name('users.index');
 Route::get('/create_user',[UserController::class,'create'])->name('users.create');
 Route::post('/store_user',[UserController::class,'store'])->name('users.store');
 Route::get('/edit_user/{id}',[UserController::class,'edit'])->name('users.edit');
 Route::put('/update_user/{id}',[UserController::class,'update'])->name('users.update');
 Route::delete('/delete_user/{id}',[UserController::class,'destroy'])->name('users.destroy');
+
+Route::resource('/categories', CategoryController::class);
